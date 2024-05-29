ac439430c8495516edd1ae1efd23100cbfe4ab7d
AlexVGC1 <91946809+AlexVGC1@users.noreply.github.com>
2024-05-29 07:09:06 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+// create web server
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+app.get('/comments', function(req, res) {
+    res.send('Hello World!');
+});
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
+// Path:
